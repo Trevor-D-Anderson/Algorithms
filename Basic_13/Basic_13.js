b7eba465e6bbf22103f350d62008f009258308f0
Trevor Anderson <tbone101@gmail.com>
2021-09-11 10:48:32 -0700

    added functions

--- Basic_13/Basic_13.js
+++ Basic_13/Basic_13.js
@@ -1,14 +1,21 @@
 // 1. Print 1-255 : Print all the integers from 1-255
-for(var x=1; x<=255; x++)
-{
-    //console.log(x);
+function print1to255(){
+    for(var x=1; x<=255; x++)
+    {
+        console.log(x);
+    }
 }
-
 // 2. Print Sum 0-255 : Print integers from 0 to 255, and with each integer print the sum so far.
-// var sum = 0;
-for(var x=0; x<=255; x++)
-{
-//     console.log(x);
-//     sum = sum + x;
-//     console.log(sum)
+function printSum1to255(){
+    var sum = 0;
+    for(var x=0; x<=255; x++)
+    {
+        console.log(x);
+        sum += x;
+        console.log(sum)
+    }
+}
+// 3. Find and Print Max : Print the largest element in a given array
+function printArrayMax(){
+
 }
