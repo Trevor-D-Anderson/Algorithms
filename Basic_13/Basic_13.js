fd3acd0329690deb334c2d4ffd84a124c838be74
Trevor Anderson <tbone101@gmail.com>
2021-09-11 10:43:04 -0700

    Finished Basic 2

--- Basic_13/Basic_13.js
+++ Basic_13/Basic_13.js
@@ -5,3 +5,10 @@ for(var x=1; x<=255; x++)
 }
 
 // 2. Print Sum 0-255 : Print integers from 0 to 255, and with each integer print the sum so far.
+// var sum = 0;
+for(var x=0; x<=255; x++)
+{
+//     console.log(x);
+//     sum = sum + x;
+//     console.log(sum)
+}
