10759580bd52749f924486d83beb98aa99778901
Trevor Anderson <tbone101@gmail.com>
2021-09-20 20:53:33 -0700

    Finished Assignment

--- Basic_13/Loop_Challenges.js
+++ Basic_13/Loop_Challenges.js
@@ -38,3 +38,11 @@ function sigma(){
 // sigma()
 
 // 5. Factorial
+function factorial(){
+    var num = 1
+    for(var x = 1; x <=12; x++){
+        num = num*x
+    }
+    console.log(num)
+}
+// factorial()
