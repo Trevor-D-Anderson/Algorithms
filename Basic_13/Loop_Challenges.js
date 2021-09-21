63c38c9b768d1bbf929e25c1ae580140f4ed5513
Trevor Anderson <tbone101@gmail.com>
2021-09-20 20:40:56 -0700

    Added Loop Challenges assignment

---
+++ Basic_13/Loop_Challenges.js
@@ -0,0 +1,11 @@
+// 1. Print odds 1-20
+function printOdds1to20(){
+    for(var x = 0; x <=20; x++){
+        if(x%2==1){
+            console.log(x)
+        }
+    }
+}
+// printOdds1to20()
+
+// 2. Decreasing Multiples of 3
